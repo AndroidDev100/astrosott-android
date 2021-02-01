93a03107a28da7834a449acc6da845828ca12c8c
Ankita Arora <ankita.arora@oyelabs.com>
2021-02-01 16:20:37 +0530

    signup ui updated

--- app/src/main/java/com/astro/sott/activities/profile/ui/EditPasswordActivity.java
+++ app/src/main/java/com/astro/sott/activities/profile/ui/EditPasswordActivity.java
@@ -19,5 +19,6 @@ public class EditPasswordActivity extends BaseBindingActivity<ActivityEditPasswo
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 }
