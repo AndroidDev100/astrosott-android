9c56d112509f31c18a123702494360e0f8b2e0d0
RahulEnveu <rahul@enveu.com>
2021-04-12 13:02:01 +0530

    commit file

--- usermanagment/build/generated/source/buildConfig/debug/com/astro/sott/usermanagment/BuildConfig.java
+++ usermanagment/build/generated/source/buildConfig/debug/com/astro/sott/usermanagment/BuildConfig.java
@@ -5,9 +5,8 @@ package com.astro.sott.usermanagment;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.astro.sott.usermanagment";
+  public static final String LIBRARY_PACKAGE_NAME = "com.astro.sott.usermanagment";
   public static final String BUILD_TYPE = "debug";
-  public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
 }
