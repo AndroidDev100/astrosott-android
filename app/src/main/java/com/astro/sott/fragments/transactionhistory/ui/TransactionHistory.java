0fd29de6a1d6bb20f09b772f4f6d5cb8430c8341
Sunny <sunny@enveu.com>
2021-04-16 13:38:23 +0530

    temp

--- app/src/main/java/com/astro/sott/fragments/transactionhistory/ui/TransactionHistory.java
+++ app/src/main/java/com/astro/sott/fragments/transactionhistory/ui/TransactionHistory.java
@@ -291,7 +291,7 @@ public class TransactionHistory extends BaseBindingFragment<FragmentTransactionH
     }
 
     private void loadDataFromModel(List<OrderItem> order, boolean checkboxVisibility) {
-        if (order!=null&&order.size() > 0) {
+        if (order != null && order.size() > 0) {
             TransactionAdapter adapter = new TransactionAdapter(TransactionHistory.this, order, checkboxVisibility);
             getBinding().recyclerView.setAdapter(adapter);
         } else {
@@ -445,7 +445,9 @@ public class TransactionHistory extends BaseBindingFragment<FragmentTransactionH
                 });
                 return getFilePathAndStatus;
             } catch (IOException e) {
-                getBinding().includeProgressbar.progressBar.setVisibility(View.GONE);
+                getActivity().runOnUiThread(() -> {
+                    getBinding().includeProgressbar.progressBar.setVisibility(View.GONE);
+                });
                 e.printStackTrace();
                 getFilePathAndStatus.filStatus = false;
                 getFilePathAndStatus.filePath = getReportPath(filename, extension);
