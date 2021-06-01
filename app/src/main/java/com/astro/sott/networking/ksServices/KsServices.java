2161c5b3ea210f51b74c287cf1be4891bb33539e
Sunny <sunny@enveu.com>
2021-06-01 15:17:15 +0530

    increase the size of pack fragment

--- app/src/main/java/com/astro/sott/networking/ksServices/KsServices.java
+++ app/src/main/java/com/astro/sott/networking/ksServices/KsServices.java
@@ -8065,7 +8065,6 @@ public class KsServices {
         clientSetupKs();
         SearchAssetFilter relatedFilter = new SearchAssetFilter();
         relatedFilter.setTypeIn(String.valueOf(mediatype));
-        relatedFilter.orderBy(SortByEnum.NAME_ASC + "");
 
         FilterPager filterPager = new FilterPager();
         filterPager.setPageIndex(1);
