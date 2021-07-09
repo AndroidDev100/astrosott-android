ba7f448c749dfb4c2bedc29f04a80343ac7edac6
sunny <sunny@enveu.com>
2021-07-09 14:51:58 +0530

    hardcoded check removed

--- app/src/main/java/com/astro/sott/activities/signUp/ui/SignUpActivity.kt
+++ app/src/main/java/com/astro/sott/activities/signUp/ui/SignUpActivity.kt
@@ -207,7 +207,7 @@ class SignUpActivity : AppCompatActivity(), AccountBlockedDialog.EditDialogListe
                         checkPasswordValidation(password)
 
                     }
-                } else if (true) {
+                } else if (emailPattern.containsMatchIn(email_mobile)) {
                     checkPassword("email", email_mobile, password)
                 } else {
 
