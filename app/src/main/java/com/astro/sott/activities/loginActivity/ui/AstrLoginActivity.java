9ef854efb52ebfe5ef03d62f89c9658ff04aeef3
sunny <sunny@enveu.com>
2021-07-21 16:19:37 +0530

    hardcoded check removed

--- app/src/main/java/com/astro/sott/activities/loginActivity/ui/AstrLoginActivity.java
+++ app/src/main/java/com/astro/sott/activities/loginActivity/ui/AstrLoginActivity.java
@@ -282,7 +282,7 @@ public class AstrLoginActivity extends BaseBindingActivity<ActivityAstrLoginBind
             //   resetPassword();
         });
         getBinding().signup.setOnClickListener(view -> {
-            Intent intent = new Intent(this,SignUpActivity.class);
+            Intent intent = new Intent(this, SignUpActivity.class);
             intent.putExtra(AppLevelConstants.FROM_KEY, from);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
@@ -586,7 +586,7 @@ public class AstrLoginActivity extends BaseBindingActivity<ActivityAstrLoginBind
                     return false;
 
                 }
-            } else if (true) {
+            } else if (email_mobile.matches(EMAIL_REGEX)) {
                 type = "Email";
                 getBinding().errorEmail.setTextColor(getResources().getColor(R.color.heather));
                 getBinding().errorEmail.setText(getResources().getString(R.string.mobile_suggestion));
