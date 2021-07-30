701e8bf6808f122cf462cd2af7a64fde7a570592
sunny <sunny@enveu.com>
2021-07-30 14:03:05 +0530

    transaction history fix

--- app/src/main/java/com/astro/sott/fragments/transactionhistory/adapter/TransactionAdapter.java
+++ app/src/main/java/com/astro/sott/fragments/transactionhistory/adapter/TransactionAdapter.java
@@ -19,6 +19,8 @@ import com.astro.sott.fragments.transactionhistory.ui.TransactionHistory;
 import com.astro.sott.usermanagment.modelClasses.getPaymentV2.OrderItem;
 import com.astro.sott.utils.helpers.AppLevelConstants;
 
+import org.apache.commons.lang3.text.WordUtils;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -60,8 +62,22 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
             } else {
                 holder.binding.currency.setText("");
             }
+
+            if (orderItems.get(position).getPaymentsInfo() != null && orderItems.get(position).getPaymentsInfo().get(0) != null && orderItems.get(position).getPaymentsInfo().get(0).getPaymentCategory() != null) {
+                try {
+                    holder.binding.paymentCategory.setText(WordUtils.capitalizeFully(orderItems.get(position).getPaymentsInfo().get(0).getPaymentCategory()));
+                } catch (Exception e) {
+                }
+            }
+
             if (orderItems.get(position).getPaymentsInfo() != null && orderItems.get(position).getPaymentsInfo().get(0) != null && orderItems.get(position).getPaymentsInfo().get(0).getPaymentType() != null) {
-                holder.binding.creditNo.setText(orderItems.get(position).getPaymentsInfo().get(0).getPaymentType());
+                if (orderItems.get(position).getPaymentsInfo().get(0).getPaymentType().equalsIgnoreCase("Credit card")) {
+                    String creditNumber = orderItems.get(position).getPaymentsInfo().get(0).getCreditCardNumber().replace("x", "");
+
+                    holder.binding.creditNo.setText(orderItems.get(position).getPaymentsInfo().get(0).getPaymentType() + " " + creditNumber);
+                } else {
+                    holder.binding.creditNo.setText(orderItems.get(position).getPaymentsInfo().get(0).getPaymentType());
+                }
             } else {
                 holder.binding.creditNo.setText("");
             }
