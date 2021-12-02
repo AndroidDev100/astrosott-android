a8fcdb1e31e84143aa371a5e391afdf0891bfa91
sunny <sunny@enveu.com>
2021-12-02 12:37:16 +0530

    Mike 1542 fixed

--- app/src/main/java/com/astro/sott/activities/home/HomeActivity.java
+++ app/src/main/java/com/astro/sott/activities/home/HomeActivity.java
@@ -120,38 +120,6 @@ public class HomeActivity extends BaseBindingActivity<ActivityHomeBinding> imple
                         switchToLiveTvFragment();
                     }
                     return true;
-                /*case R.id.navigation_video:
-                    if (videoFragment == null) {
-                        videoFragment = new VideoFragment();
-                        fragmentManager.beginTransaction().add(R.id.content_frame, videoFragment, "2").hide(videoFragment).commitAllowingStateLoss();
-                        switchToVideoFragment();
-                    } else {
-                        switchToVideoFragment();
-                    }
-                    return true;
-                case R.id.navigation_viu_apps:
-                    if (viuAppsFragment == null) {
-                        viuAppsFragment = new ViuFragmentNew();
-                        fragmentManager.beginTransaction().add(R.id.content_frame, viuAppsFragment, "4").hide(viuAppsFragment).commitAllowingStateLoss();
-                        switchToViuFragment();
-
-                    } else {
-                        switchToViuFragment();
-
-                    }
-
-                    return true;*/
-
-//                case R.id.navigation_more:
-//                    if (moreFragment == null) {
-//                        moreFragment = new MoreFragment();
-//                        fragmentManager.beginTransaction().add(R.id.content_frame, moreFragment, "5").hide(moreFragment).commitAllowingStateLoss();
-//                        switchToMoreFragment();
-//                    } else {
-//                        switchToMoreFragment();
-//                    }
-//
-//                    return true;
                 case R.id.navigation_more:
                     if (moreNewFragment == null) {
                         setProfileFragment();
@@ -227,7 +195,7 @@ public class HomeActivity extends BaseBindingActivity<ActivityHomeBinding> imple
 
     private BottomNavigationView navigation;
 
-    @SuppressLint("RestrictedApi")
+    @SuppressLint({"RestrictedApi", "WrongConstant"})
     private static void removeNavigationShiftMode(BottomNavigationView view) {
 
         BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
@@ -254,139 +222,11 @@ public class HomeActivity extends BaseBindingActivity<ActivityHomeBinding> imple
         if (getIntent().getStringExtra("fragmentType") != null)
             fragmentType = getIntent().getStringExtra("fragmentType");
         modelCall();
-        //ApplicationUpdateManager.getInstance(getApplicationContext()).setAppUpdateCallBack(this);
-        // Before starting an update, register a listener for updates.
-
-        //     ApplicationUpdateManager.getInstance(getApplicationContext()).getAppUpdateManager().registerListener(listener);
-
-        //  ApplicationUpdateManager.getInstance(getApplicationContext()).isUpdateAvailable();
-
         createViewModel();
         intializeBilling();
         setClicks();
-//        getIFAID();
-//        loadNativeAd();
     }
 
-//    private void getIFAID() {
-//        @SuppressLint("StaticFieldLeak") AsyncTask<Void, Void, String> task = new AsyncTask<Void, Void, String>() {
-//            @Override
-//            protected String doInBackground(Void... params) {
-//                AdvertisingIdClient.Info idInfo = null;
-//                try {
-//                    idInfo = AdvertisingIdClient.getAdvertisingIdInfo(getApplicationContext());
-//                } catch (GooglePlayServicesNotAvailableException e) {
-//                    e.printStackTrace();
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//                String advertId = null;
-//                try {
-//                    advertId = idInfo.getId();
-//                    Log.e("AD ID", advertId);
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//                return advertId;
-//            }
-//
-//            @Override
-//            protected void onPostExecute(String advertId) {
-//                Log.e("AD ID", advertId);
-//                Toast.makeText(HomeActivity.this, advertId, Toast.LENGTH_SHORT).show();
-//            }
-//        };
-//        task.execute();
-//    }
-
-//    private void loadNativeAd() {
-//        nativeAd = new NativeAd(this, AppConstants.FB_PLACEMENT_ID);
-//        nativeAd.setAdListener(new NativeAdListener() {
-//            @Override
-//            public void onMediaDownloaded(Ad ad) {
-//                // Native ad finished downloading all assets
-//                Log.e(TAG, "Native ad finished downloading all assets.");
-//            }
-//
-//            @Override
-//            public void onError(Ad ad, AdError adError) {
-//                // Native ad failed to load
-//                Log.e(TAG, "Native ad failed to load: " + adError.getErrorMessage());
-//            }
-//
-//            @Override
-//            public void onAdLoaded(Ad ad) {
-//                // Native ad is loaded and ready to be displayed
-//                Log.e(TAG, "Native ad is loaded and ready to be displayed!");
-//                if (nativeAd == null || nativeAd != ad) {
-//                    return;
-//                }
-//                // Inflate Native Ad into Container
-//                inflateAd(nativeAd);
-//            }
-//
-//            @Override
-//            public void onAdClicked(Ad ad) {
-//                // Native ad clicked
-//                Log.e(TAG, "Native ad clicked!");
-//            }
-//
-//            @Override
-//            public void onLoggingImpression(Ad ad) {
-//                // Native ad impression
-//                Log.e(TAG, "Native ad impression logged!");
-//            }
-//        });
-//
-//        // Request an ad
-//        nativeAd.loadAd();
-//    }
-
-    //    private void inflateAd(NativeAd nativeAd) {
-//        nativeAd.unregisterView();
-//
-//        // Add the Ad view into the ad container.
-//        nativeAdLayout = findViewById(R.id.native_ad_container);
-//        LayoutInflater inflater = LayoutInflater.from(HomeActivity.this);
-//        // Inflate the Ad view.  The layout referenced should be the one you created in the last step.
-//        adView = (LinearLayout) inflater.inflate(R.layout.layout_native_ad, nativeAdLayout, false);
-//        nativeAdLayout.addView(adView);
-//
-//        // Add the AdOptionsView
-//        LinearLayout adChoicesContainer = findViewById(R.id.ad_choices_container);
-//        AdOptionsView adOptionsView = new AdOptionsView(HomeActivity.this, nativeAd, nativeAdLayout);
-//        adChoicesContainer.removeAllViews();
-//        adChoicesContainer.addView(adOptionsView, 0);
-//
-//        // Create native UI using the ad metadata.
-//        AdIconView nativeAdIcon = adView.findViewById(R.id.native_ad_icon);
-//        TextView nativeAdTitle = adView.findViewById(R.id.native_ad_title);
-//        MediaView nativeAdMedia = adView.findViewById(R.id.native_ad_media);
-//        TextView nativeAdSocialContext = adView.findViewById(R.id.native_ad_social_context);
-//        TextView nativeAdBody = adView.findViewById(R.id.native_ad_body);
-//        TextView sponsoredLabel = adView.findViewById(R.id.native_ad_sponsored_label);
-//        Button nativeAdCallToAction = adView.findViewById(R.id.native_ad_call_to_action);
-//
-//        // Set the Text.
-//        nativeAdTitle.setText(nativeAd.getAdvertiserName());
-//        nativeAdBody.setText(nativeAd.getAdBodyText());
-//        nativeAdSocialContext.setText(nativeAd.getAdSocialContext());
-//        nativeAdCallToAction.setVisibility(nativeAd.hasCallToAction() ? View.VISIBLE : View.INVISIBLE);
-//        nativeAdCallToAction.setText(nativeAd.getAdCallToAction());
-//        sponsoredLabel.setText(nativeAd.getSponsoredTranslation());
-//
-//        // Create a list of clickable views
-//        List<View> clickableViews = new ArrayList<>();
-//        clickableViews.add(nativeAdTitle);
-//        clickableViews.add(nativeAdCallToAction);
-//
-//        // Register the Title and CTA button to listen for clicks.
-//        nativeAd.registerViewForInteraction(
-//                adView,
-//                nativeAdMedia,
-//                nativeAdIcon,
-//                clickableViews);
-//    }
     private void modelCall() {
         subscriptionViewModel = ViewModelProviders.of(this).get(SubscriptionViewModel.class);
     }
@@ -394,24 +234,13 @@ public class HomeActivity extends BaseBindingActivity<ActivityHomeBinding> imple
     private void setClicks() {
         toolbarTitle = getBinding().toolbar.findViewById(R.id.toolbar_text);
         getBinding().toolbar.setPadding(0, 0, 0, 0);
-//        getBinding().toolbar.setContentInsetsAbsolute(0,0);
         ImageView searchIcon = getBinding().toolbar.findViewById(R.id.search_icon);
-        // ImageView notification_Icon = getBinding().toolbar.findViewById(R.id.notification_icon);
         ImageView appsLaunchIcon = getBinding().toolbar.findViewById(R.id.apps_launch_icon);
         ImageView homeIcon = getBinding().toolbar.findViewById(R.id.home_icon);
-//        if(KsPreferenceKey.getInstance(this).getUserActive())
-//            notification_Icon.setVisibility(View.VISIBLE);
-//        else
-//            notification_Icon.setVisibility(View.GONE);
 
         homeIcon.setOnClickListener(view -> {
-          /*  navigation.setSelectedItemId(R.id.navigation_home);
-            homeFragment.sameClick();*/
-        });
 
-//        notification_Icon.setOnClickListener(view -> {
-//            new ActivityLauncher(HomeActivity.this).notification(HomeActivity.this, NotificationActivity.class);
-//        });
+        });
 
         searchIcon.setOnClickListener(view -> new ActivityLauncher(HomeActivity.this).searchActivity(HomeActivity.this, ActivitySearch.class));
         appsLaunchIcon.setOnClickListener(view -> {
@@ -442,9 +271,6 @@ public class HomeActivity extends BaseBindingActivity<ActivityHomeBinding> imple
         // attaching tab mediator
         new TabLayoutMediator(getBinding().tabs, getBinding().viewPager,
                 (tab, position) -> tab.setText(titles[position])).attach();
-
-//        getBinding().tabs.setupWithViewPager(getBinding().pager);
-
 
         getBinding().viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
@@ -521,11 +347,6 @@ public class HomeActivity extends BaseBindingActivity<ActivityHomeBinding> imple
     }
 
     private void switchToHomeFragment() {
-        /*if (homeFragment!=null){
-            fragmentManager.beginTransaction().hide(active).show(homeFragment).commitAllowingStateLoss();
-            checkSameClick();
-            active = homeFragment;
-        }*/
         FirebaseEventManager.getFirebaseInstance(HomeActivity.this).navEvent("Navigation", "Home");
         setToolBarScroll(1);
         setMargins(150, 0);
@@ -599,11 +420,6 @@ public class HomeActivity extends BaseBindingActivity<ActivityHomeBinding> imple
         active = videoFragment;
     }
 
-
-    //    private void switchToMoreFragment() {
-//        fragmentManager.beginTransaction().hide(active).show(moreFragment).commit();
-//        active = moreFragment;
-//    }
     private void switchToNewMoreFragment() {
         FirebaseEventManager.getFirebaseInstance(HomeActivity.this).navEvent("Navigation", "PROFILE");
         getBinding().appbar.setVisibility(View.GONE);
@@ -612,7 +428,6 @@ public class HomeActivity extends BaseBindingActivity<ActivityHomeBinding> imple
         setMargins(0, 0);
         getBinding().toolbar.setVisibility(View.GONE);
         getBinding().mainLayout.setVisibility(View.VISIBLE);
-        // getBinding().appbar.setVisibility(View.GONE);
         fragmentManager.beginTransaction().hide(active).show(moreNewFragment).commit();
         active = moreNewFragment;
 
@@ -627,9 +442,6 @@ public class HomeActivity extends BaseBindingActivity<ActivityHomeBinding> imple
     @Override
     protected void onStart() {
         super.onStart();
-
-
-//        setToHome();
     }
 
     @Override
@@ -641,22 +453,6 @@ public class HomeActivity extends BaseBindingActivity<ActivityHomeBinding> imple
     protected void onResume() {
         super.onResume();
         updateLang();
-
-        // Checks that the update is not stalled during 'onResume()'.
-        // However, you should execute this check at all entry points into the app.
-       /* ApplicationUpdateManager.getInstance(getApplicationContext()).getAppUpdateManager().getAppUpdateInfo().addOnSuccessListener(appUpdateInfo -> {
-
-            if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
-                // If the update is downloaded but not installed,
-                // notify the user to complete the update.
-
-                popupSnackbarForCompleteUpdate();
-
-
-                // When status updates are no longer needed, unregister the listener.
-                ApplicationUpdateManager.getInstance(getApplicationContext()).getAppUpdateManager().unregisterListener(listener);
-            }
-        });*/
     }
 
     private void updateLang() {
@@ -682,48 +478,12 @@ public class HomeActivity extends BaseBindingActivity<ActivityHomeBinding> imple
 
     }
 
-   /* @Override
-    public void getAppUpdateCallBack(AppUpdateInfo appUpdateInfo) {
-
-        this.appUpdateInfo = appUpdateInfo;
-        if (appUpdateInfo != null) {
-
-            ApplicationUpdateManager.getInstance(getApplicationContext()).startUpdate(appUpdateInfo, AppUpdateType.FLEXIBLE, this, ApplicationUpdateManager.APP_UPDATE_REQUEST_CODE);
-        } else {
-            PrintLogging.printLog("InApp update", "NoUpdate available");
-        }
-    }*/
-
-    /* Displays the snackbar notification and call to action. */
-   /* private void popupSnackbarForCompleteUpdate() {
-        Snackbar snackbar =
-                Snackbar.make(getBinding().mainLayout, getResources().getString(R.string.update_has_downloaded), Snackbar.LENGTH_INDEFINITE);
-        snackbar.setAction(getResources().getString(R.string.restart), view -> ApplicationUpdateManager.getInstance(getApplicationContext()).getAppUpdateManager().completeUpdate());
-        snackbar.setActionTextColor(
-                getResources().getColor(R.color.colorPrimary));
-        snackbar.show();
-    }*/
-
-    /*InstallStateUpdatedListener listener = installState -> {
-        if (installState.installStatus() == InstallStatus.DOWNLOADED) {
-            // After the update is downloaded, show a notification
-            // and request user confirmation to restart the app.
-            popupSnackbarForCompleteUpdate();
-        }
-    };*/
 
     private void intializeBilling() {
         billingProcessor = new BillingProcessor(HomeActivity.this, HomeActivity.this);
         billingProcessor.initializeBillingProcessor();
         // stopProcessor();
     }
-
-   /* @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (!billingProcessor.handleActivityResult(requestCode, resultCode, data)) {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-    }*/
 
     public void stopProcessor() {
         if (billingProcessor != null) {
@@ -749,16 +509,7 @@ public class HomeActivity extends BaseBindingActivity<ActivityHomeBinding> imple
 
     @Override
     public void onPurchasesUpdated(@NonNull BillingResult billingResult, @Nullable List<Purchase> purchases) {
-       /* if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK && purchases != null) {
-            if (purchases.get(0).getPurchaseToken() != null) {
-                if (SystemClock.elapsedRealtimeNanos() - lastClickTime < 8000) {
-                    return;
-                }
-                lastClickTime = SystemClock.elapsedRealtime();
-                if (!TabsData.getInstance().isDetail())
-                    processPurchase(purchases);
-            }
-        }*/
+
     }
 
     private void processPurchase(List<Purchase> purchases) {
